0e72e3c9a804907aa1afed6b6478b9905fc0fa64
Gilbert Terbio <124416457+Gil-WD47P@users.noreply.github.com>
2023-05-13 04:17:21 +0800

    debug payment.php

--- payment.php
+++ payment.php
@@ -11,12 +11,17 @@ session_start();
   </div>
   <div class="mx-auto text-center container">
     <p><?php if(isset($_GET['order_status'])) {echo $_GET['order_status'];} ?></p>
-    <p>&#8369;<?php if(isset($_SESSION['total'])) {echo $_SESSION['total'];} ?></p>
-    <?php if(isset($_SESSION['total'])) { ?>
-    <input class="btn btn-primary" value="Pay Now"type="submit">
-    <?php } ?>
 
-    <?php if(isset($_GET['order_status']) && $_GET['order_status']== "NOT PAID"); { ?>
+
+    <p>Total Payment:  &#8369;  <?php if(isset($_SESSION['total'])) {echo $_SESSION['total'];} ?></p>
+    
+    <?php if(isset($_SESSION['total']) && $_SESSION['total'] !=0 ){ ?>
+    <input class="btn btn-primary" value="Pay Now"type="submit">
+    <?php } else { ?>
+<p>You don't have an order</p>
+      <?php } ?>
+
+    <?php if(isset($_GET['order_status']) && $_GET['order_status']== "NOT PAID") { ?>
     <input class="btn btn-primary" value="Pay Now"type="submit">
     <?php } ?>
 
