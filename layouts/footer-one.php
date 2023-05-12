e890d2285b852ec90148ba0786e859702ade4a8e
Gilbert Terbio <124416457+Gil-WD47P@users.noreply.github.com>
2023-05-13 03:01:53 +0800

    add header-one.php and footer-one.php to all pages

---
+++ layouts/footer-one.php
@@ -0,0 +1,56 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="inner-content">
+              <p>
+                Copyright &copy; Online Home Furnishing
+                <a
+                  rel="nofollow noopener"
+                  href="https://github.com/orgs/KodeGo-Bootcamp/teams/online-home-furnishing"
+                  target="_blank"
+                  >Group 4</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <div onclick="scrollToTop()" class="scrollTop">
+      <i class="fa-solid fa-chevron-up"></i>
+    </div>
+
+<script>
+      function scrollToTop(){
+        window.scrollTo(0, 0);
+      }
+    </script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/accordions.js"></script>
+  
+
+    <script language="text/Javascript">
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t) {
+        //declaring the array outside of the
+        if (!cleared[t.id]) {
+          // function makes it static and global
+          cleared[t.id] = 1; // you could use true and false, but that's more typing
+          t.value = ""; // with more chance of typos
+          t.style.color = "#fff";
+        }
+      }
+    </script>
+  </body>
+</html>
--- register.php
+++ register.php
@@ -78,88 +78,7 @@ session_start();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta name="description" content="This is an Online Furniture Store made by Group 4 from WD47" />
-  <meta name="title" content="Online Home Furnishing" />
-  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
-    rel="stylesheet" />
-
-  <title>Online Home Furnishing</title>
-
-  <script src="https://kit.fontawesome.com/aab2d75e67.js" crossorigin="anonymous"></script>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-
-
-  <!-- Additional CSS Files -->
-  <link rel="stylesheet" href="assets/css/fontawesome.css" />
-  <link rel="stylesheet" href="assets/css/login-style.css" />
-  <link rel="stylesheet" href="assets/css/owl.css" />
-
-
-  <!-- Favicon -->
-  <link rel="icon" href="assets/images/logo-1.jpg">
-</head>
-
-<body>
-
-  <!-- ***** Preloader Start ***** -->
-  <div id="preloader">
-    <div class="jumper">
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  </div>
-  <!-- ***** Preloader End ***** -->
-
-
-
-  <!-- Header -->
-  <header class="">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">
-          <h2>Home <em>Furnishing</em></h2></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="products.html">Products</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link cart-one" href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
-            </li>
-            <li class="nav-item">
-              <a href="login.php" class="nav-link"><i class="fa-solid fa-circle-user"></i></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-
-  <body>
+<?php include('layouts/header-one.php'); ?>
 
     <!-- Register -->
     <div class="container text-center mt-3 pt-5">
@@ -195,30 +114,5 @@ session_start();
       </form>
     </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Additional Scripts -->
-    <script src="assets/js/custom.js"></script>
-    <script src="assets/js/owl.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/isotope.js"></script>
-    <script src="assets/js/accordions.js"></script>
-
-
-    <script language="text/Javascript">
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t) {
-        //declaring the array outside of the
-        if (!cleared[t.id]) {
-          // function makes it static and global
-          cleared[t.id] = 1; // you could use true and false, but that's more typing
-          t.value = ""; // with more chance of typos
-          t.style.color = "#fff";
-        }
-      }
-    </script>
-  </body>
-
-</html>
+  <?php include('layouts/footer-one.php'); ?>
