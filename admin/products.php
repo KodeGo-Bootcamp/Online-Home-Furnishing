f23271aa18042703f9068879597babcb2e4ebd0e
Gilbert Terbio <124416457+Gil-WD47P@users.noreply.github.com>
2023-06-06 07:53:03 +0800

    add edit images button

--- admin/products.php
+++ admin/products.php
@@ -100,6 +100,7 @@
               <th scope="col">Product Price</th>
               <th scope="col">Product Offer</th>
               <th scope="col">Product Category</th>
+              <th scope="col">Edit Images</th>
               <th scope="col">Edit</th>
               <th scope="col">Delete</th>
             </tr>
@@ -113,6 +114,7 @@
               <td>&#8369; <?php echo $product['product_price'];?></td>
               <td><?php echo $product['product_special_offer'] . "%";?></td>
               <td><?php echo $product['product_category'];?></td>
+              <td><a class="btn btn-warning" href="<?php echo "edit_images.php?product_id=".$product['product_id']."&product_name=".$product['product_name'];?>">Edit Images</a></td>
               <td><a class="btn btn-primary" href="edit_product.php?product_id=<?php echo $product['product_id'];?>">Edit</a></td>
               <td><a class="btn btn-danger" href="delete_product.php?product_id=<?php echo $product['product_id'];?>">Delete</a></td>
             </tr>
