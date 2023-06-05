70f6aade3827ae570b7abffe57850e528abc0520
Gilbert Terbio <124416457+Gil-WD47P@users.noreply.github.com>
2023-06-06 02:20:35 +0800

    convert index.html to index.php, add logout.php, add header.php

--- admin/login.php
+++ admin/login.php
@@ -1,62 +1,50 @@
+<?php include('header.php'); ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Group 4 from WDD47P KodeGo Bootcamp">
-    <title>Online Home Furnishing</title>
+<?php
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
+include('../server/connection.php');
 
-    
+if(isset($_SESSION['admin_logged_in'])){
+  header('location: index.php');
+  exit;
+}
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#7952b3">
+  if(isset($_POST['login_btn'])){
+
+    $email =  $_POST['email'];
+    $password =  md5($_POST['password']);
+
+    $stmt = $conn->prepare("SELECT admin_id, admin_name, admin_email, admin_password FROM admins WHERE admin_email = ? AND admin_password = ? LIMIT 1");
+
+    $stmt->bind_param('ss',$email,$password);
+
+    if($stmt->execute()){
+      $stmt->bind_result($admin_id,$admin_name,$admin_email,$admin_password);
+      $stmt->store_result();
+
+      if($stmt->num_rows() == 1){
+        $stmt->fetch();
+
+        $_SESSION['admin_id'] = $admin_id;
+        $_SESSION['admin_name'] = $admin_name;
+        $_SESSION['admin_email'] = $admin_email;
+        $_SESSION['admin_logged_in'] = true;
+
+        header('location: index.php?login_success=You logged in successfully!');
+
+    }else{
+      header('location: login.php?error=Could not verify your account');
+    }
+
+    }else{
+        //error
+        header('location: login.php?error=Something went wrong');
+    }
+
+  }
+  ?>
 
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
-    </div>
-  </div>
-</header>
 
 <div class="container-fluid">
   <div class=""  style="min-height: 1000px">
@@ -81,7 +69,7 @@
 
 
           <div class="mx-auto container">
-              <form id="login-form"  enctype="multipart/form-data">
+              <form id="login-form" method="POST" action="login.php" enctype="multipart/form-data">
                 <p style="color: red;"><?php if(isset($_GET['error'])){ echo $_GET['error']; }?></p>
                 <div class="form-group mt-2">
                     <label>Email</label>
@@ -95,7 +83,7 @@
 
 
                 <div class="form-group mt-3">
-                    <input type="submit" class="btn btn-primary" name="login" value="Login"/>
+                    <input type="submit" class="btn btn-primary" name="login_btn" value="Login"/>
                 </div>
  
               </form>
